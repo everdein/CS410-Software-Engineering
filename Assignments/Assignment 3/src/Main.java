b45a3844556a98459704f4ff7f24aa521681f34d
Matthew <everdein@gmail.com>
2019-06-06 11:07:50 -0700

    Creates Assignment 3

---
+++ Assignments/Assignment 3/src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Hello world!
+        System.out.println("Hello World!");
+
+        // Creates simple notepad object.
+        SimpleNotePad simpleNotePad = new SimpleNotePad();
+    }
+}
